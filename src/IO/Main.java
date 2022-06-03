38339286a61c941886ab6f6df4090b4cf4aa3b0d
Maks Burenko <Cherniymaks@yandex.ru>
2022-06-03 07:15:27 +0300

    123

--- src/IO/Main.java
+++ src/IO/Main.java
@@ -5,30 +5,12 @@ import java.io.InputStream;
 
 public class Main {
 
-        public static int checkSumOfStream(InputStream inputStream) throws IOException {
-            int controlSumma;
-            int n;
-            int Cn = 0;
-            int bn = 0;
-            for (int i = 0;((bn = inputStream.read()) > 0) && (i <= n-1);i++) {
-                Cn = Cn + bn;
-                if (i == n-1){
-
-                }
-            }
-            while ((bn = inputStream.read()) > 0){
-                int i = 1;
-                if (i <= n) {
-                    i = i+1;
-                   Cn = Cn + bn;
-                }
-                else {
-                    if (i == n+1) {
-                        bn = inputStream.read();              // bn + 1
-                    }
-                }
-            }
-            controlSumma = Integer.rotateLeft(Cn,1) ^ bn;
-            return controlSumma;
+    public static int checkSumOfStream(InputStream inputStream) throws IOException {
+        int Cn = 0;
+        int bn;
+        while ((bn = inputStream.read()) > -1) {
+            Cn = Integer.rotateLeft(Cn, 1) ^ bn;
         }
+        return Cn;
+    }
 }
